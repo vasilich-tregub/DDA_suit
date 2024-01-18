e342bd140c8dedb232131ed8751f71787ed1a79b
V.V.T. <vasilich.tregub@gmail.com>
2024-01-18 17:46:22 +0700

    traits enum instead of NaN

--- sdf1D/sdf1D.cpp
+++ sdf1D/sdf1D.cpp
@@ -9,25 +9,39 @@
 
 const int WIDTH = 32;
 double fld[WIDTH];
+int trait[WIDTH];
+
+enum traits
+{
+    VAR, SET, FIXED
+};
 
 int main()
 {
-    constexpr double nan = std::numeric_limits<double>::quiet_NaN();
     for (int i = 0; i < WIDTH; ++i)
     {
-        fld[i] = nan;
+        fld[i] = 0;
+        trait[i] = VAR;
     }
-    fld[WIDTH / 2] = 0.3333;
+    fld[WIDTH / 2 - 1] = 0.3333;
+    trait[WIDTH / 2 - 1] = FIXED;
     fld[WIDTH / 2] = -0.6667;
+    trait[WIDTH / 2] = FIXED;
     for (int i = 1; i < WIDTH; ++i)
     {
-        if (std::isnan(fld[i]) && !std::isnan(fld[i - 1]))
+        if (trait[i] == VAR && trait[i - 1] != VAR)
+        {
             fld[i] = fld[i - 1] - 1.0;
+            trait[i] = SET;
+        }
     }
     for (int i = WIDTH - 2; i >= 0; --i)
     {
-        if (std::isnan(fld[i]) && !std::isnan(fld[i + 1]))
+        if (trait[i] == VAR && trait[i + 1] != VAR)
+        {
             fld[i] = fld[i + 1] + 1.0;
+            trait[i] = SET;
+        }
     }
     std::string html = HTMLheader;
     for (int i = 0; i < WIDTH; ++i)
@@ -35,7 +49,7 @@ int main()
         html.append(std::format("<div title=\"{}\">{:.3f}</div>\n", fld[i], fld[i]));
     }
     html.append(HTMLfooter);
-    std::ofstream outf("page.html");
+    std::ofstream outf("sdfNumerical.html");
     outf << html;
     outf.close();
 }
